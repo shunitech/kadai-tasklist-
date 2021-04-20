401a08baa134bef42c3d985f56e5cf5d8a1a2c66
TA.training <training.t.a.2021@gmail.com>
2021-04-20 14:02:33 +0000

    CURD tasks

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -16,7 +16,7 @@
     </table>
     
     {!! link_to_route('tasks.edit', 'このタスクを編集', ['task' => $task->id], ['class' => 'btn btn-light']) !!}
-    {!! Form::model($tasks, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
     
