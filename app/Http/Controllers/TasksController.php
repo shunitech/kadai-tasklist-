e835afbdefaf211baaa8ef37662fc5bf7a9e6ba9
TA.training <training.t.a.2021@gmail.com>
2021-04-21 14:37:26 +0000

    status column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -30,10 +30,12 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $request->validate([
+            'status' => 'required|max:10',
             'content' => 'required|max:255',
         ]);
         
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
@@ -64,8 +66,14 @@ class TasksController extends Controller
 
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required|max:255',
+        ]);
+        
         $task = Task::findOrFail($id);
         
+        $task->status = $request->status; 
         $task->content = $request->content;
         $task->save();
 
